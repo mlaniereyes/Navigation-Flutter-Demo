d5565e102d9b9fbfb4e4c6455135dbb59b906f45
Melanie Reyes <mlaniereyes@gmail.com>
2020-04-24 20:55:51 -0500

    learning navigator/routes

--- lib/screen0.dart
+++ lib/screen0.dart
@@ -15,13 +15,14 @@ class Screen0 extends StatelessWidget {
               color: Colors.red,
               child: Text('Go To Screen 1'),
               onPressed: () {
-                //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
               },
             ),
             RaisedButton(
               color: Colors.blue,
               child: Text('Go To Screen 2'),
               onPressed: () {
+                Navigator.pushNamed(context, '/second');
                 //Navigate to Screen 2
               },
             ),
